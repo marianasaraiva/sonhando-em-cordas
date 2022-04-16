17d2fd790bde09eb0f49b6455a84a5dbc54f2716
Mariana Saraiva <marianascm8@gmail.com>
2022-04-16 11:10:53 -0300

    feat: initial setup

---
+++ sonhando-em-cordas/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Game from './components/Game';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div className="wrapper-container">
+      <Header/>
+      <Game/>
+    </div>
+  );
+}
+
+export default App;
